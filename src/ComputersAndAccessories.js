656aa7d0a0df892c0d1ec2624a8b4128d62ab0ac
ejiro <ejiro1994@gmail.com>
2020-09-15 01:54:33 +0100

    readme screenshot

--- src/ComputersAndAccessories.js
+++ src/ComputersAndAccessories.js
@@ -19,45 +19,45 @@ function ComputersAndAccessories() {
                         releaseDate="11 Apr 2017"
                         price={2200.29}
                         image="/assets/computers-&-accessories/imac.jpg"
-                        rating={2}
+                        rating={5}
                     />
                     <Product
-                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
-                        author="Eric Ries"
+                        title="Apple iMac Pro (27-inch with Retina 5K Display, 3.2GHz 8-core Intel Xeon W, 32GB RAM, 1TB SSD - Previous Model)"
+                        // author="Eric Ries"
                         releaseDate="6 Oct 2011"
-                        price={10.31}
+                        price={4101.97}
                         image="/assets/computers-&-accessories/imac-pro.jpg"
-                        rating={4}
+                        rating={5}
                     />
                     <Product
-                        title="Eloquent Javascript, 3rd Edition: A Modern Introduction to Programming"
-                        author="Marijn Haverbeke"
+                        title="New Apple iPad Pro (11-inch, Wi-Fi, 128GB) - Space Grey (2nd Generation)"
+                        // author="Marijn Haverbeke"
                         releaseDate="14 Dec. 2018"
-                        price={22.93}
+                        price={1069.00}
                         image="/assets/computers-&-accessories/ipad-pro.jpg"
                         rating={4}
                     />
                 </div>
                 <div className="ProductPage__row">
                     <Product
-                        title="Eloquent Javascript, 3rd Edition: A Modern Introduction to Programming"
-                        author="Marijn Haverbeke"
+                        title="New Apple MacBook Air (13-inch, 1.1GHz dual-core 10th-generation Intel Core i3 processor, 8GB RAM, 256GB) - Silver"
+                        // author="Marijn Haverbeke"
                         releaseDate="14 Dec. 2018"
                         price={22.93}
                         image="/assets/computers-&-accessories/macbook-air.jpg"
                         rating={4}
                     />
                     <Product
-                        title="This is Your Brain on Music: Understanding a Human Obsession"
-                        author="Daniel Levitin"
+                        title="Apple MacBook Pro (16-Inch, 16GB RAM, 512GB Storage) - Silver"
+                        // author="Daniel Levitin"
                         releaseDate="14 Dec. 2018"
                         price={22.93}
                         image="/assets/computers-&-accessories/macbook-pro.jpg"
                         rating={4}
                     />
                     <Product
-                        title="Start With Why: How Great Leaders Inspire Everyone To Take Action"
-                        author="Simon Sinek"
+                        title="Apple MacBook Pro (16-Inch, 16GB RAM, 512GB Storage) - Silver"
+                        // author="Simon Sinek"
                         releaseDate="6 Oct 2011"
                         price={7.38}
                         image="/assets/computers-&-accessories/macbook-16-inch.jpg"
@@ -66,7 +66,7 @@ function ComputersAndAccessories() {
                 </div>
                 <div className="ProductPage__row">
                     <Product
-                        title="elon musk"
+                        title="imac mini"
                         author="Eric Ries"
                         releaseDate="6 Oct 2011"
                         price={10.31}
